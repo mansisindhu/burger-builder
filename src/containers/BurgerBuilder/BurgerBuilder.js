0484f054e7b81a924014d0d003387e26565c0396
mansi sindhu <76626095+mansisindhu@users.noreply.github.com>
2021-05-08 15:28:12 +0530

    added a modal window using different component

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,8 @@ import React, { Component } from "react";
 import Aux from "../../hoc/Aus";
 import Burger from "../../components/Burger/Burger";
 import BuilderControls from "../../components/Burger/Buildcontrols/BuildControls";
+import Modal from '../../components/UI/Modal/Modal';
+import OrderSummary from '../../components/Burger/OrderSummary/Ordersummary';
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -20,7 +22,8 @@ class BurgerBuilder extends Component {
       meat: 0,
     },
     totalPrice: 4,
-    purchasable: false
+    purchasable: false,
+    purchasing: false
   };
 
 
@@ -67,6 +70,12 @@ class BurgerBuilder extends Component {
     this.updatePurchaseState(updatedIngredients);
   };
 
+
+  purchaseHandler = () => {
+    this.setState({purchasing: true});
+  }
+
+
   render() {
         const disabledInfo = {
             ...this.state.ingredients
@@ -76,12 +85,16 @@ class BurgerBuilder extends Component {
         }
     return (
       <Aux>
+        <Modal show={this.state.purchasing}>
+          <OrderSummary ingredients={this.state.ingredients}/>
+        </Modal>
         <Burger ingredients={this.state.ingredients} />
         <BuilderControls 
         ingredientAdded={this.addIngredientHandler}
         ingredientRemoved={this.removeIngredientHandler}
         disabled={disabledInfo}
         purchasable={this.state.purchasable}
+        ordered={this.purchaseHandler}
         price={this.state.totalPrice}/>
       </Aux>
     );
